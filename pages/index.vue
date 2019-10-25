0ce67936287c4585a8f246615b50cdb72018ce76
Wellington Aquino <wellingtonm.aquino@gmail.com>
2019-10-25 16:56:49 -0400

    start navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,87 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        flip-conta
-      </h1>
-      <h2 class="subtitle">
-        flipconta
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+<nav class="navbar is-link">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="~@/assets/img/flipconta-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+    </a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   </div>
+
+  <div id="navbarHome" class="navbar-menu">
+    <div class="navbar-end">
+      <a href="#incio" class="navbar-item">
+        início
+      </a>
+      <a href="#solucoes" class="navbar-item">
+        soluções
+      </a>
+      <a href="#preco" class="navbar-item">
+        preço
+      </a>
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link is-arrowless" href="https://bulma.io/documentation/overview/start/">
+          suporte
+        </a>
+        <div class="navbar-dropdown is-boxed">
+          <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
+            Overview
+          </a>
+          <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
+            Modifiers
+          </a>
+          <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+            Columns
+          </a>
+          <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+            Layout
+          </a>
+          <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+            Form
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+            Elements
+          </a>
+          <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+            Components
+          </a>
+        </div>
+      </div>
+      <a href="#facebook" class="navbar-item">
+        F
+      </a>
+      <a href="#twiter" class="navbar-item">
+        T
+      </a>
+      <a href="#abrir-conta" class="navbar-item">
+        Abrir minha conta grátis
+      </a>
+      <a href="#login" class="navbar-item">
+        Logar na minha conta
+      </a>
+      <a href="#consultor" class="navbar-item">
+        Seja consultor
+      </a>
+    </div>
+  </div>
+</nav>
+
+<section class="hero is-link">
+  <div class="hero-body">
+    <p class="title">
+      Documentation
+    </p>
+    <p class="subtitle">
+      Everything you need to <strong>create a website</strong> with Bulma
+    </p>
+  </div>
+</section>
+</div>
 </template>
 
 <script>
@@ -39,34 +95,4 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
