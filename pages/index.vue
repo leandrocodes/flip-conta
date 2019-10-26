9fb9fc55bfc2baa650a0fa3e18f3ebe04d2f6c7e
leandrocodes <leaxviana140@gmail.com>
2019-10-26 00:12:27 -0400

    buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,92 @@
 <template>
-<section class="hero is-primary is-large">
-  <!-- Hero head: will stick at the top -->
-  <div class="hero-head">
-    <nav class="navbar is-primary is-transparent">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="#">
-          <img src="~@/assets/img/flipconta-logo-white.png" alt="Flip Conta" width="112" height="28">
-        </a>
-        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-          <span></span>
-          <span></span>
-          <span></span>
+  <section class="hero is-primary is-large">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+      <nav class="navbar is-primary is-transparent">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <img src="~@/assets/img/flipconta-logo-white.png" alt="Flip Conta" width="112" height="28" />
+          </a>
+          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-      </div>
 
-      <div id="navbarHome" class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <a href="" class="button is-link is-outlined is-rounded">início</a>
-          </div>
-          <div class="navbar-item">
-            <a href="" class="button is-link is-outlined is-rounded">soluções</a>
-          </div>
-          <div class="navbar-item">
-            <a href="" class="button is-link is-outlined is-rounded">preços</a>
-          </div>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <div class="navbar-link is-arrowless is-rounded">
-              <a href="" class="button is-link is-outlined is-rounded">suporte</a>
+        <div id="navbarHome" class="navbar-menu">
+          <div class="navbar-end">
+            <div class="nav-link navbar-item">
+              <a href class="button is-light is-outlined is-rounded">início</a>
             </div>
-            <div class="navbar-dropdown is-boxed">
-              <a class="navbar-item has-text-dark" href="">
-                Chat
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item has-text-dark" href="">
-                Fone
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item has-text-dark" href="">
-                contato@flipconta.com
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item has-text-dark" href="">
-                <span>
-                  Horário de Funcionamento<br><br>
+            <div class="nav-link navbar-item">
+              <a href class="button is-light is-outlined is-rounded">soluções</a>
+            </div>
+            <div class="nav-link navbar-item">
+              <a href class="button is-light is-outlined is-rounded">preços</a>
+            </div>
+            <div class="nav-link navbar-item has-dropdown is-hoverable">
+              <div class="navbar-link is-arrowless is-rounded">
+                <a href class="button is-light is-outlined is-rounded">suporte</a>
+              </div>
+              <div class="navbar-dropdown is-boxed">
+                <a class="navbar-item has-text-dark" href>Chat</a>
+                <hr class="navbar-divider" />
+                <a class="navbar-item has-text-dark" href>Fone</a>
+                <hr class="navbar-divider" />
+                <a class="navbar-item has-text-dark" href>contato@flipconta.com</a>
+                <hr class="navbar-divider" />
+                <a class="navbar-item has-text-dark" href>
                   <span>
-                    Segunda à Sexta<br>
-                    8h30 às 19h30<br><br>
-                    Sábado<br>
-                    9h30 às 19h30<br><br>
+                    Horário de Funcionamento
+                    <br />
+                    <br />
                     <span>
-                      Exceto Domingos e Feriados
+                      Segunda à Sexta
+                      <br />8h30 às 19h30
+                      <br />
+                      <br />Sábado
+                      <br />9h30 às 19h30
+                      <br />
+                      <br />
+                      <span>Exceto Domingos e Feriados</span>
                     </span>
                   </span>
-                </span>    
+                </a>
+              </div>
+            </div>
+            <div class="navbar-item">
+              <a href="#facebook" class="fb-link button is-rounded is-light is-outlined social">
+                <font-awesome-icon :icon="['fab', 'facebook-f']" />
               </a>
             </div>
-          </div>
-          <div class="navbar-item">
-            <a href="#facebook" class="button is-rounded is-link is-outlined social">
-              <font-awesome-icon :icon="['fab', 'facebook-f']"/>
-            </a>
-          </div>
-          <div class="navbar-item">
-            <a href="#twitter" class="button is-rounded is-link is-outlined social">
-              <font-awesome-icon :icon="['fab', 'twitter']"/>
-            </a>
-          </div>
-          <div class="navbar-item">
-            <a href="#abrir-conta" class="button is-link is-outlined is-rounded">Abrir minha conta grátis</a>
-          </div>
-          <div class="navbar-item">
-            <a href="#login" class="button is-link is-outlined is-rounded">Logar na minha conta</a>
-          </div>
-          <div class="navbar-item">
-            <a href="#consultor" class="button is-link is-outlined is-rounded">Seja consultor</a>
+            <div class="navbar-item">
+              <a href="#twitter" class="twt-link button is-rounded is-light is-outlined social">
+                <font-awesome-icon :icon="['fab', 'twitter']" />
+              </a>
+            </div>
+            <div class="navbar-item">
+              <a href="#abrir-conta" class="button is-light is-outlined is-rounded">Abrir minha conta grátis</a>
+            </div>
+            <div class="navbar-item">
+              <a href="#login" class="button is-light is-outlined is-rounded">Logar na minha conta</a>
+            </div>
+            <div class="navbar-item">
+              <a href="#consultor" class="button is-light is-outlined is-rounded">Seja consultor</a>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
 
-  <div class="hero-body">
-    <p class="title">
-      FLIPCONTA
-    </p>
-    <p class="subtitle">
-      Lorem ipsum dolor <strong>sit amet</strong>.
-    </p>
-  </div>
-
-</section>
+    <div class="hero-body">
+      <p class="title">FLIPCONTA</p>
+      <p class="subtitle">
+        Lorem ipsum dolor
+        <strong>sit amet</strong>.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -100,17 +95,24 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    mouseInHover(event) {
+      event.target.classList.remove('is-light')
+      event.target.classList.add('is-primary')
+    },
+    mouseOutHover(event) {
+      event.target.classList.remove('is-primary')
+      event.target.classList.add('is-light')
+    }
   }
 }
 </script>
 
 <style>
-  .navbar-dropdown .navbar-item:hover{
-    color:purple!important;
-  }
-  .social{
-    width: 2.25em;
-    height: 2.25em;
-  }
-
 </style>
