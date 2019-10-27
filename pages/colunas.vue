85e29e969c6ebe0c4f06d79cae085f7888b7d754
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-27 20:46:41 -0300

    update contact 2

---
+++ pages/colunas.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="columns">
+      <div class="column">
+        First column
+      </div>
+      <div class="column">
+        Second column
+      </div>
+      <div class="column">
+        Third column
+      </div>
+      <div class="column">
+        Fourth column
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
