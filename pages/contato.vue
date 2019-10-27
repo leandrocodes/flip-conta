476608c9181a5ddf3a451c44dab6c7f85d857140
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-27 11:40:54 -0300

    add contato vue

---
+++ pages/contato.vue
@@ -0,0 +1,23 @@
+<template>
+    <barra-menu-secundaria></barra-menu-secundaria>
+</template>
+
+<script>
+
+import BarraMenuSecundaria from '~/components/barra-menu-secundaria.vue'
+
+export default {
+    components: {
+        BarraMenuSecundaria
+    },
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
