8d0f610f80feb3268cb27087584fb51a041f0cbc
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-27 15:00:16 -0300

    add contact column

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <section class="hero is-primary is-bold contact">
+    <div class='contact'>
+        <section class="hero is-primary is-bold">
             <div class="hero-head">
                 <barra-menu-secundaria></barra-menu-secundaria>
             </div>
@@ -11,40 +11,56 @@
                 </p>
             </div>
         </section>
-        <form>
-            <div class="texto-entrada has-text-centered">
-                <h1 class="title has-text-link">Fale Conosco</h1>
-                <h3 class="subtitle has-text-link">
-                Tire dúvidas, elogios e mais
-                </h3>
+        <div class="columns">
+            <div class="column">
+                First column
             </div>
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <input class="input" type="text" placeholder="Nome">
+            <div class="column">
+                Second column
+            </div>
+            <div class="column">
+                Third column
+            </div>
+            <div class="column">
+                Fourth column
+            </div>
+        </div>
+        <!-- <div class="columns">
+            <div class="column is-half">
+                <div class="texto-entrada has-text-centered">
+                    <h1 class="title has-text-link">Fale Conosco</h1>
+                    <h3 class="subtitle has-text-link">
+                    Tire dúvidas, elogios e mais Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus blanditiis sint ipsam ullam, perspiciatis suscipit. Accusamus doloremque quaerat rem dolor vero rerum consequuntur, id voluptas illo voluptatum quo itaque ab!]
+                    </h3>
                 </div>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Email">
+                <div class="field is-grouped is-grouped-centered">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Nome">
+                    </div>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Email">
+                    </div>
+                </div>
+                <div class="field is-grouped is-grouped-centered">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Celular">
+                    </div>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Assunto">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <textarea class="textarea" placeholder="e.g. Hello world">Sua Mensagem</textarea>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <a href class="button is-link is-rounded">Enviar Mensagem</a>
+                    </div>
                 </div>
             </div>
-            <div class="field is-grouped is-grouped-centered">
-                <div class="control">
-                    <input class="input" type="text" placeholder="Celular">
-                </div>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Assunto">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <textarea class="textarea" placeholder="e.g. Hello world">Sua Mensagem</textarea>
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <a href class="button is-link is-rounded">Enviar Mensagem</a>
-                </div>
-            </div>
-        </form>
+        </div> -->
     </div>    
     </template>
 
