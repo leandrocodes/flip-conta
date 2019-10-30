78a43815b1fbd7c6552f9cfa34f90211856b4650
leandrocodes <leaxviana140@gmail.com>
2019-10-29 21:07:46 -0400

    footer 1st part

--- components/rodape.vue
+++ components/rodape.vue
@@ -1,6 +1,18 @@
 <template>
-  <footer>
-    rodapé cabuloso
+  <footer class="has-text-light">
+    <div class="container">
+      <div class="contact-footer columns is-centered">
+        <div class="column is-4 has-text-centered">
+          <p> <font-awesome-icon :icon="['fas', 'headset']" /> | 123 456 789 </p>
+        </div>
+        <div class="column is-4 has-text-centered">
+          <p> <font-awesome-icon :icon="['fas', 'envelope']" /> | contato@flipconta.com.br </p>
+        </div>
+        <div class="column is-4 has-text-centered">
+          <p> <font-awesome-icon :icon="['fas', 'map-marker-alt']" /> | raja, 123 - belo horizonte </p>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
