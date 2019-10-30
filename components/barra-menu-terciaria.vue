6a8ab0ac3423c1affc1579210669a11e7beb4843
Wellington Aquino <wellingtonm.aquino@gmail.com>
2019-10-29 21:54:05 -0400

    create cadastro_anunciante vue and scss

---
+++ components/barra-menu-terciaria.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar is-terciaria is-transparent">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        <img src="~@/assets/img/flipconta-logo-white.png" alt="Flip Conta" width="112" height="28" />
+      </a>
+      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navbarHome" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <a href="#login" class="button is-link is-outlined is-rounded">Logar na minha conta</a>
+        </div>
+        <div class="navbar-item">
+          <a href="#consultor" class="button is-link is-outlined is-rounded">Seja consultor</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ pages/cadastro_anunciante.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="anunciante">
+    <section class="hero is-bold">
+      <div class="hero-head">
+        <barra-menu-terciaria></barra-menu-terciaria>
+      </div>
+      <div class="hero-body">
+        <p class="subtitle is-6 has-text-centered-mobile">
+          você está em >>
+          <strong>cadastro do consultor</strong>
+          <!-- Colocar no media -->
+        </p>
+      </div>
+    </section>
+    <div class="columns is-centered is-mobile">
+      <div class="column is-10-mobile is-8-tablet is-4-desktop is-3-fullhd">
+        <div class="texto-inicio has-text-centered">
+          <h3 class="subtitle has-text-weight-semibold">Insire seus dados para o cadastro</h3>
+        </div>
+        <form>
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu nome completo" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu email" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input is-primary is-rounded" type="text" placeholder="Telefone" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input is-primary is-rounded" type="text" placeholder="Cidade" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <input class="input is-primary is-rounded" type="text" placeholder="Numero de notas emitidas mês?" />
+            </div>
+          </div>
+          <div class="field">
+            <div class="control is-expanded">
+              <a href class="button is-primary is-rounded">Enviar Mensagem</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+import BarraMenuTerciaria from '~/components/barra-menu-terciaria.vue'
+
+export default {
+  components: {
+    BarraMenuTerciaria
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
