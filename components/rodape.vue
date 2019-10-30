b0a776c615cbb583e483a204c6a4ea5c4a0e302e
leandrocodes <leaxviana140@gmail.com>
2019-10-29 21:34:22 -0400

    nossos serviços from footer

--- components/rodape.vue
+++ components/rodape.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="has-text-light">
     <div class="container">
-      <div class="contact-footer columns is-centered">
+      <div class="columns is-centered">
         <div class="column is-4 has-text-centered">
           <p> <font-awesome-icon :icon="['fas', 'headset']" /> | 123 456 789 </p>
         </div>
@@ -10,6 +10,25 @@
         </div>
         <div class="column is-4 has-text-centered">
           <p> <font-awesome-icon :icon="['fas', 'map-marker-alt']" /> | raja, 123 - belo horizonte </p>
+        </div>
+      </div>
+      <div class="columns is-centered">
+        <div class="column is-2">
+          <h3 class="is-uppercase has-text-left">nossos serviços</h3>
+          <div class="text" style="margin-top: 15px;">
+            <a class="has-text-light" href="">boletos</a> <br>
+            <a class="has-text-light" href="">notas fiscais</a> <br>
+            <a class="has-text-light" href="">data programada</a> <br>
+            <a class="has-text-light" href="">boletos</a> <br>
+            <a class="has-text-light" href="">notas fiscais</a> <br>
+            <a class="has-text-light" href="">data programada</a>
+          </div>
+        </div>
+        <div class="column is-2 is-offset-2">
+          <h3 class="is-uppercase has-text-left">sobre nós</h3>
+        </div>
+        <div class="column is-2 is-offset-2">
+          <h3 class="is-uppercase has-text-left">nossos serviços</h3>
         </div>
       </div>
     </div>
