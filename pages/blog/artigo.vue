fa57c7ca5f59df2c2d862074a37bcae8e79c7439
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-31 17:57:47 -0300

    final lead 2

--- pages/blog/artigo.vue
+++ pages/blog/artigo.vue
@@ -11,8 +11,8 @@
               <div class="column is-4-mobile is-1-desktop has-text-centered"><span class="is-size-7-mobile">15/06/2018</span></div>
               <div class="column is-4-mobile is-1-desktop has-text-centered"><span class="is-size-7-mobile">578</span></div>
             </div>
-            <h1 class="title is-size-5 is-size-5-mobile has-text-centered">O recurso de controle de contas a pagar do nosso Sistema ERP em Nuvem, centralizando</h1>
-            <h2 class="subtitle is-size-6 is-size-6-mobile has-text-centered">O recurso de controle de contas a pagar do nosso Sistema ERP em Nuvem, permite que você e sua equipe façam projeções de gastos e investimentos, centralizando informações sobre cliente, fornecedores, vencimentos e muito mais.</h2>
+            <h1 class="title is-size-2-desktop is-size-5-mobile has-text-centered">O recurso de controle de contas a pagar do nosso Sistema ERP em Nuvem, centralizando</h1>
+            <h2 class="subtitle is-size-4-desktop is-size-6-mobile has-text-centered has-text-white-ter">O recurso de controle de contas a pagar do nosso Sistema ERP em Nuvem, permite que você e sua equipe façam projeções de gastos e investimentos, centralizando informações sobre cliente, fornecedores, vencimentos e muito mais.</h2>
         </div>
         <!-- <p class="subtitle is-6 has-text-centered-mobile">
           você está em >>
