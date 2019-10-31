4f029f41db3be49813275a41f411895dbaeef26d
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-31 15:54:39 -0300

    upadate cliente cadastro add footer

--- pages/cliente/cadastro.vue
+++ pages/cliente/cadastro.vue
@@ -11,107 +11,112 @@
           <!-- Colocar no media -->
         </p>
       </div>
-    </section>
-    <div v-if="fisica" class="columns is-centered is-mobile">
-      <div class="column is-9-mobile is-6-tablet is-3-desktop is-3-fullhd">
-        <div class="texto-inicio has-text-centered">
-          <h4 class="subtitle is-black has-text-weight-bold">Pessoa Física</h4>
+    </section>]
+    <div class="conteudo">
+      <div v-if="fisica" class="columns is-centered is-mobile">
+        <div class="column is-9-mobile is-6-tablet is-3-desktop is-3-fullhd">
+          <div class="texto-inicio has-text-centered">
+            <h4 class="subtitle is-black has-text-weight-bold">Pessoa Física</h4>
+          </div>
+          <form>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Insira seu nome completo" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Insira seu email" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Telefone" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Cidade" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Numero de notas emitidas mês?" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <a href class="button is-primary is-rounded">Cadastrar</a>
+              </div>
+            </div>
+          </form>
         </div>
-        <form>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu nome completo" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu email" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Telefone" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Cidade" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Numero de notas emitidas mês?" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <a href class="button is-primary is-rounded">Cadastrar</a>
-            </div>
-          </div>
-        </form>
       </div>
-    </div>
-    <div v-else class="columns is-centered is-mobile">
-      <div class="column is-9-mobile is-6-tablet is-3-desktop is-3-fullhd">
-        <div class="texto-inicio has-text-centered">
-          <h4 class="subtitle is-black has-text-weight-bold">Pessoa Jurídica</h4>
+      <div v-else class="columns is-centered is-mobile">
+        <div class="column is-9-mobile is-6-tablet is-3-desktop is-3-fullhd">
+          <div class="texto-inicio has-text-centered">
+            <h4 class="subtitle is-black has-text-weight-bold">Pessoa Jurídica</h4>
+          </div>
+          <form>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Insira seu nome completo" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Insira seu email" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Telefone" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Cidade" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <input class="input is-primary is-rounded" type="text" placeholder="Numero de notas emitidas mês?" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control is-expanded">
+                <a href class="button is-primary is-rounded">Cadastrar</a>
+              </div>
+            </div>
+          </form>
         </div>
-        <form>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu nome completo" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Insira seu email" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Telefone" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Cidade" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <input class="input is-primary is-rounded" type="text" placeholder="Numero de notas emitidas mês?" />
-            </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <a href class="button is-primary is-rounded">Cadastrar</a>
-            </div>
-          </div>
-        </form>
       </div>
-    </div>
 
-    <div class="modelo-bg">
-      <img src="~@/assets/img/modelo-cliente.png" alt />
+      <div class="modelo-bg">
+        <img src="~@/assets/img/modelo-cliente.png" alt />
+      </div>
+      <div class="tabs">
+        <div class="tab has-text-weight-semibold" @click="fisica=true">Pessoa Física</div>
+        <div class="tab has-text-weight-semibold" @click="fisica=false">Pessoa Jurídica</div>
+      </div>
     </div>
-    <div class="tabs">
-      <div class="tab has-text-weight-semibold" @click="fisica=true">Pessoa Física</div>
-      <div class="tab has-text-weight-semibold" @click="fisica=false">Pessoa Jurídica</div>
-    </div>
+    <rodape></rodape>
   </div>
 </template>
 
 <script>
 
 import BarraMenuTerciaria from '~/components/barra-menu-terciaria.vue'
+import Rodape from '~/components/rodape.vue'
 
 export default {
   components: {
-    BarraMenuTerciaria
+    BarraMenuTerciaria,
+    Rodape
   },
   data() {
     return {
-      fisica: true
+
     }
   }
 }
