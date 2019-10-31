3aac35a3e63ba267d58c129f7e2231000e2305e0
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-31 14:44:01 -0300

    pre-final conteudo div

--- pages/contato.vue
+++ pages/contato.vue
@@ -11,71 +11,74 @@
         </p>
       </div>
     </section>
-    <div class="columns first is-centered is-mobile">
-      <div class="column is-10-mobile is-8-tablet is-4-desktop is-3-fullhd">
-        <div class="texto-inicio has-text-centered">
-          <h1 class="title has-text-primary has-text-weight-bold">Fale Conosco</h1>
-          <h3 class="subtitle has-text-weight-semibold">Tire dúvidas, elogios e mais</h3>
-        </div>
-        <form>
-          <div class="field is-grouped is-grouped-centered">
-            <div class="control is-expanded">
-              <input class="input" type="text" placeholder="Nome" />
-            </div>
-            <div class="control is-expanded">
-              <input class="input" type="text" placeholder="Email" />
-            </div>
+    <div class="conteudo">
+      <div class="columns first is-centered is-mobile">
+        <div class="column is-10-mobile is-8-tablet is-4-desktop is-3-fullhd">
+          <div class="texto-inicio has-text-centered">
+            <h1 class="title has-text-primary has-text-weight-bold">Fale Conosco</h1>
+            <h3 class="subtitle has-text-weight-semibold">Tire dúvidas, elogios e mais</h3>
           </div>
-          <div class="field is-grouped is-grouped-centered">
-            <div class="control is-expanded">
-              <input class="input" type="text" placeholder="Celular" />
+          <form>
+            <div class="field is-grouped is-grouped-centered">
+              <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Nome" />
+              </div>
+              <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Email" />
+              </div>
             </div>
-            <div class="control is-expanded">
-              <input class="input" type="text" placeholder="Assunto" />
+            <div class="field is-grouped is-grouped-centered">
+              <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Celular" />
+              </div>
+              <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Assunto" />
+              </div>
             </div>
-          </div>
-          <div class="field">
-            <div class="control is-expanded">
-              <textarea class="textarea" placeholder="Sua Mensagem"></textarea>
+            <div class="field">
+              <div class="control is-expanded">
+                <textarea class="textarea" placeholder="Sua Mensagem"></textarea>
+              </div>
             </div>
-          </div>
-          <div class="field">
-            <div class="control">
-              <a href class="button is-link is-rounded">Enviar Mensagem</a>
+            <div class="field">
+              <div class="control">
+                <a href class="button is-link is-rounded">Enviar Mensagem</a>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <div class="columns second has-text-centered is-centered is-mobile is-multiline ">
-      
-      <div class="column is-10-mobile is-8-tablet is-2-desktop">
-        <div class="circle">
-          <font-awesome-icon :icon="['fas', 'envelope']" />
+          </form>
         </div>
-        <h1 class="title  has-text-black has-text-weight-bold">Email</h1>
-        <h2 class="subtitle has-text-weight-semibold">admin@evolta.com</h2>
-        <h2 class="subtitle has-text-weight-semibold">info@evolta.com</h2>
-      </div>
-      <div class="column is-10-mobile is-8-tablet is-2-desktop">
-        <div class="circle">
-          <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
-        </div>
-        <h1 class="title has-text-black has-text-weight-bold">Localização</h1>
-        <h2 class="subtitle has-text-weight-semibold">2750 Quadra Steet Victoria, New York</h2>
-        <h2 class="subtitle has-text-weight-semibold">USA</h2>
-      </div>
-      <div class="column is-10-mobile is-8-tablet is-2-desktop">
-        <div class="circle">
-          <font-awesome-icon :icon="['fas', 'phone-alt']" />
-        </div>
-        <h1 class="title has-text-black has-text-weight-bold">Telefone</h1>
-        <h2 class="subtitle has-text-weight-semibold">(99) 99999 9999</h2>
-        <h2 class="subtitle has-text-weight-semibold">(99) 99999 9999</h2>
       </div>
 
+      <div class="columns second has-text-centered is-centered is-mobile is-multiline ">
+        
+        <div class="column is-10-mobile is-8-tablet is-2-desktop">
+          <div class="circle">
+            <font-awesome-icon :icon="['fas', 'envelope']" />
+          </div>
+          <h1 class="title  has-text-black has-text-weight-bold">Email</h1>
+          <h2 class="subtitle has-text-weight-semibold">admin@evolta.com</h2>
+          <h2 class="subtitle has-text-weight-semibold">info@evolta.com</h2>
+        </div>
+        <div class="column is-10-mobile is-8-tablet is-2-desktop">
+          <div class="circle">
+            <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
+          </div>
+          <h1 class="title has-text-black has-text-weight-bold">Localização</h1>
+          <h2 class="subtitle has-text-weight-semibold">2750 Quadra Steet Victoria, New York</h2>
+          <h2 class="subtitle has-text-weight-semibold">USA</h2>
+        </div>
+        <div class="column is-10-mobile is-8-tablet is-2-desktop">
+          <div class="circle">
+            <font-awesome-icon :icon="['fas', 'phone-alt']" />
+          </div>
+          <h1 class="title has-text-black has-text-weight-bold">Telefone</h1>
+          <h2 class="subtitle has-text-weight-semibold">(99) 99999 9999</h2>
+          <h2 class="subtitle has-text-weight-semibold">(99) 99999 9999</h2>
+        </div>
+
+      </div>
     </div>
+    <rodape></rodape>
 
   </div>
 
@@ -84,10 +87,12 @@
 <script>
 
 import BarraMenuSecundaria from '~/components/barra-menu-secundaria.vue'
+import Rodape from '~/components/rodape.vue'
 
 export default {
   components: {
-    BarraMenuSecundaria
+    BarraMenuSecundaria,
+    Rodape
   },
   data() {
     return {
--- pages/perguntas.vue
+++ pages/perguntas.vue
@@ -11,77 +11,83 @@
         </p>
       </div>
     </section>
-    <div class="columns is-centered is-mobile">
-      <div class="column is-10-mobile is-8-tablet is-4-desktop is-3-fullhd">
-        <div class="texto-inicio has-text-centered">
-          <h1 class="title has-text-primary has-text-weight-bold">Perguntas Frequentes</h1>
+    <div class="conteudo">
+      <div class="columns is-centered is-mobile">
+        <div class="column is-10-mobile is-8-tablet is-4-desktop is-3-fullhd">
+          <div class="texto-inicio has-text-centered">
+            <h1 class="title has-text-primary has-text-weight-bold">Perguntas Frequentes</h1>
+          </div>
+          <div class="contents">
+              <h5 class="subtitle is-5 has-text-black has-text-weight-bold">License</h5>
+              <div class="card">
+                  <header class="card-header">
+                      <p class="card-header-title has-text-primary">
+                      Como funciona a polica de ressarcimento?
+                      </p>
+                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
+                          <font-awesome-icon :icon="['fas', 'minus']" />
+                      </a>
+                  </header>
+                  <div class="card-content">
+                      <div class="content has-text-justified">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.
+                      </div>
+                  </div>
+              </div>
+              <hr class="card-divider"/>
+              <div class="card">
+                  <header class="card-header">
+                      <p class="card-header-title has-text-primary">
+                      Quantos boletos posso efetuar mensalmente?
+                      </p>
+                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
+                          <font-awesome-icon :icon="['fas', 'plus']" />
+                      </a>
+                  </header>
+              </div>    
+              <hr class="card-divider"/>
+              <div class="card">
+                  <header class="card-header">
+                      <p class="card-header-title has-text-primary">
+                      Como entrar em contato?
+                      </p>
+                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
+                          <font-awesome-icon :icon="['fas', 'plus']" />
+                      </a>
+                  </header>
+              </div>
+              <hr class="card-divider"/>
+              <div class="card">
+                  <header class="card-header">
+                      <p class="card-header-title has-text-primary">
+                      Não consigo entrar na minha conta. E agora?
+                      </p>
+                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
+                          <font-awesome-icon :icon="['fas', 'plus']" />
+                      </a>
+                  </header>
+              </div>  
+          </div>
         </div>
-        <div class="contents">
-            <h5 class="subtitle is-5 has-text-black has-text-weight-bold">License</h5>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title has-text-primary">
-                    Como funciona a polica de ressarcimento?
-                    </p>
-                    <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                        <font-awesome-icon :icon="['fas', 'minus']" />
-                    </a>
-                </header>
-                <div class="card-content">
-                    <div class="content has-text-justified">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.
-                    </div>
-                </div>
-            </div>
-            <hr class="card-divider"/>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title has-text-primary">
-                    Quantos boletos posso efetuar mensalmente?
-                    </p>
-                    <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                        <font-awesome-icon :icon="['fas', 'plus']" />
-                    </a>
-                </header>
-            </div>    
-            <hr class="card-divider"/>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title has-text-primary">
-                    Como entrar em contato?
-                    </p>
-                    <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                        <font-awesome-icon :icon="['fas', 'plus']" />
-                    </a>
-                </header>
-            </div>
-            <hr class="card-divider"/>
-            <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title has-text-primary">
-                    Não consigo entrar na minha conta. E agora?
-                    </p>
-                    <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                        <font-awesome-icon :icon="['fas', 'plus']" />
-                    </a>
-                </header>
-            </div>  
-        </div>
-      </div>
-    </div>  
+      </div>  
+    </div> 
+    <rodape></rodape>
   </div>
 </template>
 
 <script>
 
 import BarraMenuSecundaria from '~/components/barra-menu-secundaria.vue'
+import Rodape from '~/components/rodape.vue'
 
 export default {
-    components: {
-        BarraMenuSecundaria
-    },
-    data() {
-        return {
+  components: {
+    BarraMenuSecundaria,
+    Rodape
+  },
+  data() {
+    return {
+
     }
   }
 }
