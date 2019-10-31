ccaea26bcf2cb68751eb73e042804fe98c606a6d
Wellington Marques <wellingtonm.aquino@gmail.com>
2019-10-31 16:47:13 -0300

    add artigo vue and scss

---
+++ pages/blog/artigo.vue
@@ -0,0 +1,257 @@
+<template>
+  <div class="artigo">
+    <section class="hero is-bold">
+      <div class="hero-head">
+        <barra-menu-secundaria></barra-menu-secundaria>
+      </div>
+      <div class="hero-body">
+        <p class="subtitle is-6 has-text-centered-mobile">
+          você está em >>
+          <strong>ler blog</strong>
+          <!-- Colocar no media -->
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-centered is-mobile">
+        <div class="column blog-posts is-11-mobile is-12-tablet is-8-desktop">
+            <div class="columns is-centered is-mobile is-multiline">
+                <div class="column is-12-mobile is-3-desktop">
+
+                    <div class="card">
+                        <div class="card-image">
+                            <div class="data has-text-centered">
+                                <span class="title is-3 has-text-weight-bold">16</span> 
+                                <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                            </div>
+                            <figure class="image is-16by9">
+                            <img src="https://bulma.io/images/placeholders/640x360.png">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="columns is-centered is-mobile is-multiline"> 
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12-mobile is-3-desktop">
+                <div class="card">
+                    <div class="card-image">
+                        <div class="data has-text-centered">
+                            <span class="title is-3 has-text-weight-bold">16</span> 
+                            <span class="subtitle is-3 has-text-weight-bold">AGO</span>
+                        </div>
+                        <figure class="image is-16by9">
+                        <img src="https://bulma.io/images/placeholders/640x360.png">
+                        </figure>
+                    </div>
+                        <div class="card-content">
+                            <div class="tag">
+                                <a href="#" class="subtitle is-6">Events</a>
+                                <a href="#" class="subtitle is-6">Design</a>
+                            </div>
+
+                            <a href="#" class="content">
+                                <span class="title is-6 has-text-weight-bold">Make your Marketing website</span> 
+                                <br>
+                                <span class="subtitle is-6 has-text-justified">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <rodape></rodape>
+
+  </div>
+
+</template>
+
+<script>
+
+import BarraMenuSecundaria from '~/components/barra-menu-secundaria.vue'
+import Rodape from '~/components/rodape.vue'
+
+export default {
+  components: {
+    BarraMenuSecundaria,
+    Rodape
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
