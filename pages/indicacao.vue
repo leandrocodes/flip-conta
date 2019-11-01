4d6751238073e528e37c0334c091aca9641342e9
leandrocodes <leaxviana140@gmail.com>
2019-11-01 13:15:32 -0400

    list blue dots

--- pages/indicacao.vue
+++ pages/indicacao.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="indicacao">
     <section class="hero indication">
       <div class="hero-head">
         <barra-menu-terciaria></barra-menu-terciaria>
@@ -9,17 +10,39 @@
             <div class="column is-offset-2 is-5 has-text-left">
               <p class="has-text-light">código do convite</p>
               <a class="button is-light has-text-weight-bold">CHARLES101</a>
-              <p class="is-size-4 has-text-white has-text-weight-bold"> <span>Charles</span> convidou você <br> para conhecer a flipconta. </p>
-              <p class="is-size-4 has-text-white has-text-weight-bold">  uma simplicidade na sua gestão.</p>
+              <p class="is-size-4 has-text-white has-text-weight-bold">
+                <span>Charles</span> convidou você
+                <br />para conhecer a flipconta.
+              </p>
+              <p class="is-size-4 has-text-white has-text-weight-bold">uma simplicidade na sua gestão.</p>
               <a class="button is-primary has-text-white has-text-weight-semibold">cadastrar</a>
             </div>
           </div>
         </div>
       </div>
       <div class="modelo-convite">
-        <img src="~@/assets/img/modelo_convite.png" alt="" srcset="">
+        <img src="~@/assets/img/modelo_convite.png" alt srcset />
       </div>
     </section>
+    
+    <div class="container cadastro">
+      <div class="columns is-multiline">
+        <div class="column is-12-desktop has-text-grey-dark">
+          <p class="">Cadastre-se agora.</p>
+        </div>
+        <div class="column is-12-desktop has-text-grey-dark">
+          <ol>
+            <li>Entre no site e baixe o app;</li>
+            <li>Escolha a opção TENHO UM CONVITE;</li>
+            <li>Use o código CHARLES101;</li>
+            <li>Faça o cadastro;</li>
+            <li>Pronto! Você e a pessoa que te indicou ganha dinheiro! ;)</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -32,5 +55,4 @@ export default {
 </script>
 
 <style>
-
 </style>
