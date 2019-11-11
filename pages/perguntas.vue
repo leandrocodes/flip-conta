a9cfaa8340eabfbd437a59c80927a636c44fca1a
leandrocodes <leaxviana140@gmail.com>
2019-11-10 22:44:44 -0400

    all click on perguntas are now completely working :D

--- pages/perguntas.vue
+++ pages/perguntas.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="questions">
-      <section class="hero">
+    <section class="hero">
       <div class="hero-head">
         <barra-menu-secundaria></barra-menu-secundaria>
       </div>
@@ -18,63 +18,77 @@
             <h1 class="title has-text-primary has-text-weight-bold">Perguntas Frequentes</h1>
           </div>
           <div class="contents">
-              <h5 class="subtitle is-5 has-text-black has-text-weight-bold">License</h5>
-              <div class="card">
-                  <header class="card-header">
-                      <p class="card-header-title has-text-primary">
-                      Como funciona a polica de ressarcimento?
-                      </p>
-                      <a @click="resposta1 = !resposta1" class="card-header-icon has-text-primary" aria-label="Resposta">
-                          <font-awesome-icon v-if="resposta1 === true" :icon="['fas', 'plus']" />
-                          <font-awesome-icon v-else :icon="['fas', 'minus']" />
-                      </a>
-                  </header>
-                  <div class="card-content">
-                      <div v-if="resposta1 === true" class="content has-text-justified">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.
-                      </div>
-                  </div>
+            <h5 class="subtitle is-5 has-text-black has-text-weight-bold">License</h5>
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title has-text-primary">Como funciona a polica de ressarcimento?</p>
+                <a @click="resposta1 = !resposta1" class="card-header-icon has-text-primary" aria-label="Resposta">
+                  <font-awesome-icon v-if="resposta1 === true" :icon="['fas', 'minus']" />
+                  <font-awesome-icon v-else :icon="['fas', 'plus']" />
+                </a>
+              </header>
+              <div class="card-content">
+                <div
+                  v-if="resposta1 === true"
+                  class="content has-text-justified"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.</div>
               </div>
-              <hr class="card-divider"/>
-              <div class="card">
-                  <header class="card-header">
-                      <p class="card-header-title has-text-primary">
-                      Quantos boletos posso efetuar mensalmente?
-                      </p>
-                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                          <font-awesome-icon :icon="['fas', 'plus']" />
-                      </a>
-                  </header>
-              </div>    
-              <hr class="card-divider"/>
-              <div class="card">
-                  <header class="card-header">
-                      <p class="card-header-title has-text-primary">
-                      Como entrar em contato?
-                      </p>
-                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                          <font-awesome-icon :icon="['fas', 'plus']" />
-                      </a>
-                  </header>
+            </div>
+            <hr class="card-divider" />
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title has-text-primary">Quantos boletos posso efetuar mensalmente?</p>
+                <a @click="resposta2 = !resposta2" class="card-header-icon has-text-primary" aria-label="Resposta">
+                  <font-awesome-icon v-if="resposta2 === true" :icon="['fas', 'minus']" />
+                  <font-awesome-icon v-else :icon="['fas', 'plus']" />
+                </a>
+              </header>
+              <div class="card-content">
+                <div
+                  v-if="resposta2 === true"
+                  class="content has-text-justified"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.</div>
               </div>
-              <hr class="card-divider"/>
-              <div class="card">
-                  <header class="card-header">
-                      <p class="card-header-title has-text-primary">
-                      Não consigo entrar na minha conta. E agora?
-                      </p>
-                      <a href="#" class="card-header-icon has-text-primary" aria-label="Resposta">
-                          <font-awesome-icon :icon="['fas', 'plus']" />
-                      </a>
-                  </header>
-              </div>  
+            </div>
+            <hr class="card-divider" />
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title has-text-primary">Como entrar em contato?</p>
+                <a @click="resposta3 = !resposta3" class="card-header-icon has-text-primary" aria-label="Resposta">
+                  <font-awesome-icon v-if="resposta3 === true" :icon="['fas', 'minus']" />
+                  <font-awesome-icon v-else :icon="['fas', 'plus']" />
+                </a>
+              </header>
+              <div class="card-content">
+                <div
+                  v-if="resposta3 === true"
+                  class="content has-text-justified"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.</div>
+              </div>
+            </div>
+            <hr class="card-divider" />
+            <div class="card">
+              <header class="card-header">
+                <p class="card-header-title has-text-primary">Não consigo entrar em minha conta. E agora?</p>
+                <a @click="resposta4 = !resposta4" class="card-header-icon has-text-primary" aria-label="Resposta">
+                  <font-awesome-icon v-if="resposta4 === true" :icon="['fas', 'minus']" />
+                  <font-awesome-icon v-else :icon="['fas', 'plus']" />
+                </a>
+              </header>
+              <div class="card-content">
+                <div
+                  v-if="resposta4 === true"
+                  class="content has-text-justified"
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla impedit ducimus, odio quos eius excepturi enim itaque accusamus dolore nostrum eligendi velit veritatis iure sequi at, nesciunt repellendus soluta perspiciatis.</div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>  
+      </div>
     </div>
     <div class="questions-img">
-      <img src="~@/assets/img/perguntas.png" alt="" srcset="">
-    </div> 
+      <img src="~@/assets/img/perguntas.png" alt srcset />
+    </div>
     <rodape></rodape>
   </div>
 </template>
@@ -98,5 +112,4 @@ export default {
 </script>
 
 <style>
-
 </style>
