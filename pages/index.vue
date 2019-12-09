c1aa40c3b07f54acd8c5fd0f504e852eaf7d1d52
leandrocodes <leaxviana140@gmail.com>
2019-12-09 13:49:28 -0400

    fix for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@
     </section>
 
     <div class="cards" id="solutions">
-      <h2 class="subtitle is-size-5-mobile has-text-right-desktop has-text-right-widescreen has-text-right-fullhd has-text-centered-mobile-only">
+      <h2 class="subtitle is-size-5-mobile has-text-centered-mobile has-text-right-desktop">
         Oferecemos as
         <br />
         <span class="is-size-7-mobile">melhores soluções para sua empresa</span>
