6bc070f14ef5c622016a8054413ff6d5faddb28d
leandrocodes <leaxviana140@gmail.com>
2019-12-16 19:51:34 -0400

    also cairo

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,21 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Cairo:200,300,400,600,700,900&display=swap');
-html {
+html,
+body {
   font-family: 'Cairo', sans-serif;
 }
 </style>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.title,
+      link: [
+        {rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Cairo:200,300,400,600,700,900&display=swap'}
+      ]
+    }
+  }
+}
+</script>
