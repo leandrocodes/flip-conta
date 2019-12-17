096c72ba2327ef75f644d1934335594ae1942e5a
leandrocodes <leaxviana140@gmail.com>
2019-12-17 11:23:55 -0400

    teciary mobile

--- components/barra-menu-terciaria.vue
+++ components/barra-menu-terciaria.vue
@@ -4,14 +4,14 @@
       <nuxt-link to="/" class="navbar-item" href="#">
         <img src="~@/assets/img/flipconta-logo-white.png" alt="Flip Conta" width="112" height="28" />
       </nuxt-link>
-      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <div class="navbar-burger burger"  @click="showNav = !showNav" :class="{ 'is-active': showNav }">
         <span></span>
         <span></span>
         <span></span>
       </div>
     </div>
 
-    <div id="navbarHome" class="navbar-menu">
+    <div id="navbarHome" class="navbar-menu" :class="{ 'is-active': showNav }">
       <div class="navbar-end">
         <div class="navbar-item">
           <nuxt-link to="/login" class="button is-primary is-outlined is-rounded entrar">Logar na minha conta</nuxt-link>
@@ -26,7 +26,11 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      showNav: false
+    }
+  }
 }
 </script>
 
